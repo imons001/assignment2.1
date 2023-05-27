017a3c0878f9dcc0db6e78da61383779baed23dc
imons001 <94793634+imons001@users.noreply.github.com>
2023-05-27 18:57:47 -0400

    Update ordering.h

--- ordering.h
+++ ordering.h
@@ -20,7 +20,7 @@ int shiftIntoPosition (Element* array, int N)
 {
   int k = N-1;
   while (k > 0 && array[k] < array[k-1])
-    {
+    {C:\Users\16513\Desktop\cs360\assingment2.1\assingment2.1.cbp
       std::swap(array[k], array[k-1]);
       --k;
     }
@@ -29,7 +29,22 @@ int shiftIntoPosition (Element* array, int N)
 
 template<typename iterator>
 iterator shiftIntoPosition (iterator start, iterator stop);
-
+{
+Iterator current = stop;
+--current;
+if (current == start)
+    return start;
+Iterator previous = current;
+--previous;
+while (current != start && *current < *previous)
+{
+    std::swap(*current, *previous);
+    current = previous;
+    if (current != start)
+        --previous;
+}
+return current;
+}
 
 
 #endif
